84a7601d169444e0130eb052a44be4336aaacd84
qingang <qingang@qq.com>
2020-08-29 14:56:33 +0800

    eureka

---
+++ springcloud-service-feign/src/main/java/com/sunyard/springcloudservicefeign/SpringcloudServiceFeignApplication.java
@@ -0,0 +1,13 @@
+package com.sunyard.springcloudservicefeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudServiceFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudServiceFeignApplication.class, args);
+    }
+
+}
---
+++ springcloud-service-feign/src/test/java/com/sunyard/springcloudservicefeign/SpringcloudServiceFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyard.springcloudservicefeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudServiceFeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
